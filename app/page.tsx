8acbe213ffa8705b7993bd124062e5f955b8056d
L4R424 <125917142+L4R424@users.noreply.github.com>
2023-05-31 13:20:29 -0700

    cambio2

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,76 @@
 "use client"
 import  React, { useState } from 'react';
 
+// Definición de la clase Nodo
+class Nodo {
+  public data: any;
+  public derecha: Nodo | null;
+
+  constructor(data: any) {
+    this.data = data;
+    this.derecha = null;
+  }
+}
 
 const HomePage = () => {
-  const[nombre,setNombre]=useState('');
-const[edad,setEdad]=useState('');
-const[correo,setCorreo]=useState('');
-const[genero,setGenero]=useState('');
-const[mensaje,setMensaje]=useState('');
+  // Estado para almacenar los datos del formulario
+  const [formData, setFormData] = useState({
+    nombre: '',
+    edad: '',
+    email: '',
+    genero: ''
+  });
 
-function capturar(){
+  // Estado para almacenar la lista enlazada
+  const [listaEnlazada, setListaEnlazada] = useState<Nodo | null>(null);
 
-  setMensaje(nombre+
-    '\n'+edad+
-    '\n'+correo+
-    '\n'+genero);
- 
-}
-const formHandler = (e:any) =>{
-  e.preventDefault();
-  capturar();
-}
+  // Función para manejar los cambios en los inputs del formulario
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  // Función para manejar el envío del formulario
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { nombre, edad, email, genero } = formData;
+    
+    // Crear un nuevo nodo con los datos del formulario
+    const nodo = new Nodo({ nombre, edad, email, genero });
+    
+    // Insertar el nodo al final de la lista enlazada
+    if (!listaEnlazada) {
+      setListaEnlazada(nodo);
+    } else {
+      let temp = listaEnlazada;
+      while (temp?.derecha !== null) {
+        temp = temp.derecha;
+      }
+      temp.derecha = nodo;
+    }
+    
+    // Restablecer los valores del formulario a vacío
+    setFormData({
+      nombre: '',
+      edad: '',
+      email: '',
+      genero: ''
+    });
+  };
+
+  // Función para imprimir la lista enlazada
+  const imprimirListaEnlazada = () => {
+    let temp = listaEnlazada;
+    let valores = '';
+
+    // Recorrer la lista enlazada y concatenar los valores de cada nodo
+    while (temp !== null) {
+      valores += `Nombre: ${temp.data.nombre}, Edad: ${temp.data.edad}, Email: ${temp.data.email}, Género: ${temp.data.genero}\n`;
+      temp = temp.derecha;
+    }
+    
+    // Mostrar una alerta con los valores de la lista enlazada
+    alert(valores);
+  };
 
   return (
     <main>
@@ -29,27 +79,64 @@ const formHandler = (e:any) =>{
       </header>
 
       <section className='flex flex-col items-center mt-8'>
-        <form className='flex flex-col w-64 space-y-4 animate-fadeIn' onSubmit={formHandler}>
+        <form className='flex flex-col w-64 space-y-4 animate-fadeIn' onSubmit={handleSubmit}>
           <label className='uppercase text-xs text-purple-600'>Nombre</label>
-          <input className='border py-2 px-3 focus:outline-none focus:ring-2 ring-purple-500' type='text' placeholder='Nombre' value={(nombre)} onChange={(event)=> setNombre(event.target.value)} />
+          <input
+            className='border py-2 px-3 focus:outline-none focus:ring-2 ring-purple-500'
+            type='text'
+            name='nombre'
+            placeholder='Nombre'
+            value={formData.nombre}
+            onChange={handleChange}
+          />
 
           <label className='uppercase text-xs text-pink-600'>Edad</label>
-          <input className='border py-2 px-3 focus:outline-none focus:ring-2 ring-pink-500' type='text' placeholder='Edad' value={(edad)} onChange={(event)=> setEdad(event.target.value)} />
+          <input
+            className='border py-2 px-3 focus:outline-none focus:ring-2 ring-pink-500'
+            type='text'
+            name='edad'
+            placeholder='Edad'
+            value={formData.edad}
+            onChange={handleChange}
+          />
 
           <label className='uppercase text-xs text-blue-600'>Email</label>
-          <input className='border py-2 px-3 focus:outline-none focus:ring-2 ring-blue-500' type='text' placeholder='Email' value={(correo)} onChange={(event)=> setCorreo(event.target.value)} />
+          <input
+            className='border py-2 px-3 focus:outline-none focus:ring-2 ring-blue-500'
+            type='text'
+            name='email'
+            placeholder='Email'
+            value={formData.email}
+            onChange={handleChange}
+          />
 
           <label className='uppercase text-xs text-green-600'>Género</label>
-          <input className='border py-2 px-3 focus:outline-none focus:ring-2 ring-green-500' type='text' placeholder='Género' value={(genero)} onChange={(event)=> setGenero(event.target.value)} />
+          <input
+            className='border py-2 px-3 focus:outline-none focus:ring-2 ring-green-500'
+            type='text'
+            name='genero'
+            placeholder='Género'
+            value={formData.genero}
+            onChange={handleChange}
+          />
+
+          <button
+            className="bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4 transition-colors duration-300 ease-in-out transform hover:scale-105"
+            type="submit"
+          >
+            Enviar
+          </button>
         </form>
 
-        <button onClick={capturar} className="bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4 transition-colors duration-300 ease-in-out transform hover:scale-105">
-          capturar
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4 transition-colors duration-300 ease-in-out transform hover:scale-105"
+          onClick={imprimirListaEnlazada}
+        >
+          Imprimir Lista Enlazada
         </button>
-        <p>{mensaje}</p>
       </section>
     </main>
   );
 }
 
-export default HomePage
+export default HomePage;
